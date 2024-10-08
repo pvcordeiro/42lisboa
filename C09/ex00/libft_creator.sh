c15175eb99debc4288065819763631becd265d0a
Paulo De Souza Cordeiro <paude-so@c1r5s6.42lisboa.com>
2024-10-08 15:25:22 +0100

    finished C09 ex00

---
+++ C09/ex00/libft_creator.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cc -c -Wall -Wextra -Werror ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
+
+ar rcs libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+
+rm -f *.o
